032d3da1c9d00277fb2c548318953027862d0d32
jose <jose.silva2000@etec.sp.gov.br>
2022-09-25 21:45:39 -0300

    resolução da atividade

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("----Vivendo de dividendos----");
+Console.ResetColor();
+
+Console.Write("\nGastos mensais (em R$): ");
+decimal gastos = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Rendimentos mensais (%): ");
+decimal rendimentos = Convert.ToDecimal(Console.ReadLine());
+
+decimal resultado = gastos/(rendimentos/100);
+
+
+Console.WriteLine($"\nPara receber {gastos:C} por mês com rendimentos de {rendimentos:N1}% você precisa ter investido {resultado:C}.\n");
+
